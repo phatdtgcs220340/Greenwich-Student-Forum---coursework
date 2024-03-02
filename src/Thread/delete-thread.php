0f6104cddbcc525267cf10512d7dd503ef6709b2
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-03 00:41:33 +0700

    delete form action

---
+++ src/Thread/delete-thread.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+  header('Location: ./auth/login.php');
+  exit;
+}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $thread_id = $_POST['thread_id'];
+    
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=cw-student-forum-db', 'root', '');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt = $pdo->prepare('SELECT * FROM `thread` WHERE thread_id = ?');
+        $stmt->execute([$thread_id]);
+        $fileToDelete = '../'.$stmt->fetch(PDO::FETCH_ASSOC)['image'];
+        $stmt = $pdo->prepare('DELETE FROM `thread` WHERE thread_id = ?');
+        $stmt->execute([$thread_id]);
+
+        // Check if the file exists before attempting to delete it
+        if (file_exists($fileToDelete)) {
+            // Attempt to delete the file
+            if (unlink($fileToDelete)) {
+                echo "File $fileToDelete has been successfully deleted.";
+            } else {
+                echo "Error: Unable to delete $fileToDelete.";
+            }
+        } else {
+            echo "File $fileToDelete does not exist.";
+        }
+        header("Location: ../home-view.php");
+
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
