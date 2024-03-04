e2f85dbdd23b09dc63f7ed5dcee95ce72a5e2215
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-04 20:55:33 +0700

    add enum category 'No Category'

--- src/sql/thread-ddl.sql
+++ src/sql/thread-ddl.sql
@@ -5,7 +5,7 @@ CREATE TABLE Thread (
     image VARCHAR(255) DEFAULT NULL,
     creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     user_id INT(11),
-    category ENUM('Algorithm', 'Data Science', 'Front-end', 'Back-end', 'Database', 'Network', 'Blockchain') NOT NULL,
+    category ENUM('No Category', 'Algorithm', 'Data Science', 'Front-end', 'Back-end', 'Database', 'Network', 'Blockchain') NOT NULL,
     PRIMARY KEY (thread_id),
     FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE
 );
