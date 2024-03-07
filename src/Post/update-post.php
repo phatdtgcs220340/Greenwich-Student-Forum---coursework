3a48b0a3b17b3d90fb08b5549eca6f710134be0b
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-07 21:32:19 +0700

    action handle update post

---
+++ src/Post/update-post.php
@@ -0,0 +1,23 @@
+<?php
+    session_start();
+    if (!isset($_SESSION['user_id'])) {
+      header('Location: ./auth/login.php');
+      exit;
+    }
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $post_id = $_POST['post_id'];
+        $content = $_POST['content'];
+        $thread_id = $_POST['thread_id'];
+        try {
+            date_default_timezone_set('Asia/Ho_Chi_Minh');
+            $pdo = new PDO('mysql:host=localhost;dbname=cw-student-forum-db', 'root', '');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $pdo->prepare('UPDATE `post` SET content = ?, creation_date = ? WHERE post_id = ?');
+            $stmt->execute([$content, date('Y-m-d H:i:s'), $post_id]);
+    
+            header("Location: ../Thread/page.php?threadId=".$thread_id);
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+?>
