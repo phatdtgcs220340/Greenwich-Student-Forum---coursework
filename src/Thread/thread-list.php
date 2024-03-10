602360bd4a9f0ad1dc1adae1cbf5a9fed476a0c2
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-10 14:19:16 +0700

    add filterByCategory

--- src/Thread/thread-list.php
+++ src/Thread/thread-list.php
@@ -64,15 +64,33 @@ function threadListByUser($userId)
         exit;
     }
 }
-function threadListCategory($category)
+function threadListCategory()
 {
     try {
         $pdo = new PDO('mysql:host=localhost;dbname=cw-student-forum-db', 'root', '');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stmt = $pdo->prepare('SELECT * FROM `thread` WHERE category = ? ORDER BY creation_date DESC');
-        $stmt->execute([$category]);
+        $results_per_page = 5;
+        // Determine the current page
+        if (!isset($_GET['page'])) {
+            $page = 1;
+        } else {
+            $page = $_GET['page'];
+        }
+        $category = $_GET['filterBy'];
+        // Calculate the starting limit for the query
+        $start_limit = ($page - 1) * $results_per_page;
+        $stmt = $pdo->prepare('SELECT * FROM `thread` WHERE category = :category ORDER BY creation_date DESC LIMIT :start_limit, :results_per_page');
+        $stmt->bindParam(':category', $category, PDO::PARAM_STR);
+        $stmt->bindParam(':start_limit', $start_limit, PDO::PARAM_INT);
+        $stmt->bindParam(':results_per_page', $results_per_page, PDO::PARAM_INT);
+        $stmt->execute();
         $thread_list = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        return $thread_list;
+        // Pagination links
+        $stmt = $pdo->prepare("SELECT COUNT(*) AS total FROM `thread` WHERE category = ?");
+        $stmt->execute([$category]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $total_pages = ceil($row["total"] / $results_per_page);
+        return ["thread_list" => $thread_list, "total_pages" => $total_pages];
     } catch (PDOException $e) {
         header("HTTP/1.0 500 Internal Server Error");
         exit;
