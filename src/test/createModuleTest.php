80815bf5858f2fa055e20953798cb7bc7e1df031
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-10 23:21:05 +0700

    test

---
+++ src/test/createModuleTest.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test</title>
+</head>
+<body>
+    <form action="../module/create-module.php" method="post">
+        <input type="text" name="module_name" value="Java">
+        <button type="submit">submit</button>
+    </form>
+</body>
+</html>
