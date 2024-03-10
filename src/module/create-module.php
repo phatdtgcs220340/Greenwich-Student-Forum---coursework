c1101984014a7052206515a2e1c4be2fbf45face
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-10 23:20:46 +0700

    create module handler

---
+++ src/module/create-module.php
@@ -0,0 +1,15 @@
+<?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $moduleName = $_POST['module_name'];
+        try {
+            $pdo = new PDO('mysql:host=localhost;dbname=cw-student-forum-db', 'root', '');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $pdo->prepare('INSERT INTO `module`(module_name) VALUES(?)');
+            $stmt->execute([$moduleName]);
+            echo "successful add module";
+        } catch (PDOException $e) {
+            header("HTTP/1.0 500 Internal Server Error");
+            exit;
+        }
+    }
+?>
