36307fa62ba62f8ffa9ea04f377eedb595c5ba70
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-13 00:12:55 +0700

    change appearance and add fast answer, display total answers

--- src/main/Thread/thread.php
+++ src/main/Thread/thread.php
@@ -154,7 +154,7 @@ class Thread
         echo '
             <div class= "p-4 flex-initial w-full mb-4 bg-red-200 rounded-lg shadow">'.$user_card.'
                 <a href="' .$currentPath.$this->toThreadViewUrl() . '">
-                    <div class="w-full bg-yellow-100 hover:bg-red-100 rounded-lg shadow hover:bg-gray-100">
+                    <div class="w-full bg-yellow-100 hover:bg-blue-100 rounded-lg shadow hover:bg-gray-100">
                         <img class="rounded-t-lg" src="../' . $this->image . '" alt="" />
                         <div class="p-5">
                             <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">' . $this->title. '</h5>
@@ -165,9 +165,45 @@ class Thread
             if ($displayUser)
                 echo '
                 <div class="flex items-end mt-2 gap-2">
-                    <img class="h-5" src="../resource/static/images/icon/comment.png">
+                    <button data-modal-target="fast-answer-modal-'.$this->threadId.'" data-modal-toggle="fast-answer-modal-'.$this->threadId.'">
+                    <img class="h-6 p-1 bg-yellow-200 rounded-md border border-gray-700 hover:bg-yellow-300 hover:border-gray-800" src="../resource/static/images/icon/comment.png">
+                    <button>
                     <h5 class="text-sm font-medium">'.$extraInfo['comment'].'</h5>
-                </div>';
+                    
+                </div>
+                <!-- Main modal -->
+                <div id="fast-answer-modal-'.$this->threadId.'" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                    <div class="relative p-4 w-full max-w-md max-h-full">
+                        <!-- Modal content -->
+                        <div class="relative bg-blue-100 rounded-lg shadow">
+                            <!-- Modal header -->
+                            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+                                <h3 class="text-xl font-semibold text-gray-900">
+                                    Fast answer
+                                </h3>
+                                <button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="fast-answer-modal-'.$this->threadId.'">
+                                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                    </svg>
+                                    <span class="sr-only">Close modal</span>
+                                </button>
+                            </div>
+                            <!-- Modal body -->
+                            <div class="p-4 md:p-5">
+                                <form class="space-y-4" action="Post/create-post.php" method="post">
+                                    <div>
+                                        <label for="content" class="block mb-2 text-sm font-medium text-gray-900">Your answer</label>
+                                        <textarea name="content" id="content" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required></textarea>
+                                        <input name="user_id" value="'.$_SESSION['user_id'].'" class="hidden">
+                                        <input name="thread_id" value="'.$this->threadId.'" class="hidden">
+                                        </div>
+                                    <button type="submit" class="w-full text-gray-700 bg-red-100 hover:bg-red-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Submit</button>
+                                    
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+            </div> ';
             echo '</div>';
     }
 }
