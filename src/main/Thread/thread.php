f8a2f44f002c3abc8086c1c7c84c12f22ec42a8a
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-14 14:48:28 +0700

    remove fast answer and add solved or unsolve tag

--- src/main/Thread/thread.php
+++ src/main/Thread/thread.php
@@ -163,47 +163,17 @@ class Thread
                     </div>
                 </a>';
             if ($displayUser)
-                echo '
-                <div class="flex items-end mt-2 gap-2">
-                    <button data-modal-target="fast-answer-modal-'.$this->threadId.'" data-modal-toggle="fast-answer-modal-'.$this->threadId.'">
-                    <img class="h-6 p-1 bg-yellow-200 rounded-md border border-gray-700 hover:bg-yellow-300 hover:border-gray-800" src="../resource/static/images/icon/comment.png">
-                    <button>
-                    <h5 class="text-sm font-medium">'.$extraInfo['comment'].'</h5>
-                    
-                </div>
-                <!-- Main modal -->
-                <div id="fast-answer-modal-'.$this->threadId.'" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                    <div class="relative p-4 w-full max-w-md max-h-full">
-                        <!-- Modal content -->
-                        <div class="relative bg-blue-100 rounded-lg shadow">
-                            <!-- Modal header -->
-                            <div class="flex items-center justify-between p-4 md:p-5 border-b-2 border-yellow-100 rounded-t">
-                                <h3 class="text-xl font-semibold text-gray-900">
-                                    Fast answer
-                                </h3>
-                                <button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="fast-answer-modal-'.$this->threadId.'">
-                                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                                    </svg>
-                                    <span class="sr-only">Close modal</span>
-                                </button>
-                            </div>
-                            <!-- Modal body -->
-                            <div class="p-4 md:p-5">
-                                <form class="space-y-4" action="Post/create-post.php" method="post">
-                                    <div>
-                                        <label for="content" class="block mb-2 text-sm font-medium text-gray-900">Your answer</label>
-                                        <textarea name="content" id="content" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required></textarea>
-                                        <input name="user_id" value="'.$_SESSION['user_id'].'" class="hidden">
-                                        <input name="thread_id" value="'.$this->threadId.'" class="hidden">
-                                        </div>
-                                    <button type="submit" class="w-full text-gray-700 bg-red-100 hover:bg-red-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Submit</button>
-                                    
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-            </div> ';
-            echo '</div>';
+                if ($extraInfo['comment'] > 0)
+                    echo '
+                    <div class="flex rounded-lg items-center justify-center p-1 mt-2 gap-2 bg-green-300 w-1/5">
+                        <img class="h-4" src="../resource/static/images/icon/checked.png">
+                        <h5 class="text-sm text-white font-medium">'.$extraInfo['comment'].' answers</h5>
+                    </div>';
+                else 
+                    echo '
+                    <div class="flex items-center justify-center rounded-lg p-1 mt-2 gap-2 bg-gray-400 w-1/5">
+                        <h5 class="text-sm text-white font-medium">Unsolved</h5>
+                    </div>';
+            echo '</div> ';
     }
 }
