36f8894204b310c7472c15c61d3f4593b535078e
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-19 23:14:42 +0700

    add description

--- src/main/module/create-module.php
+++ src/main/module/create-module.php
@@ -1,11 +1,14 @@
 <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $moduleName = $_POST['module_name'];
+        $description = $_POST['description'];
         try {
             $pdo = new PDO('mysql:host=localhost;dbname=cw-student-forum-db', 'root', '');
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $stmt = $pdo->prepare('INSERT INTO `module`(module_name) VALUES(?)');
-            $stmt->execute([$moduleName]);
+            $stmt = $pdo->prepare('SELECT ) VALUES(?, ?)');
+            $stmt->execute([$moduleName, $description]);
+            $stmt = $pdo->prepare('INSERT INTO `module`(module_name, description) VALUES(?, ?)');
+            $stmt->execute([$moduleName, $description]);
             echo "successful add module";
         } catch (PDOException $e) {
             header("Location: ../error/database-connection-failed.php");
