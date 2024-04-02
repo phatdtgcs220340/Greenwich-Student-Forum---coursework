d7b3bef6683ce8b1d847a0e9c0724a4162e3e6be
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-04-02 10:42:20 +0700

    400 bad request

---
+++ src/main/error/illegal-request.php
@@ -0,0 +1,15 @@
+<?php 
+http_response_code(400);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Request</title>
+</head>
+<body>
+    <h1>You requested for what? 🫨</h1>
+    <a href="../index.php">Home</a>
+</body>
+</html>
