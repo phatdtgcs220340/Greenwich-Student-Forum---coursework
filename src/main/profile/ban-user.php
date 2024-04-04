376e2250a07eb530eb4c8a9f93b554093d64b9c2
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-04-04 22:50:37 +0700

    handle ban user

--- src/main/profile/ban-user.php
+++ src/main/profile/ban-user.php
@@ -1,11 +1,12 @@
 <?php 
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
         $userId = $_POST['user_id']; 
+        $banned = $_POST['banned'] == 1 ? 0 : 1;
         try {
             $pdo = new PDO('mysql:host=localhost;dbname=cw-student-forum-db', 'root', '');
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $stmt = $pdo->prepare('DELETE FROM `user` WHERE user_id = ?');
-            $stmt->execute([$userId]);
+            $stmt = $pdo->prepare('UPDATE `user` SET is_enabled = ? WHERE user_id = ?');
+            $stmt->execute([$banned, $userId]);
             header("Location: ../admin/user-manager.php");
         } catch (PDOException $e) {
             header("Location: ../error/database-connection-failed.php");
