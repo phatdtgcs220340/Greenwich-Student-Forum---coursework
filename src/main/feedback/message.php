ed03934e4c31d9a0b3cee24b51fc6e80a05963a3
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-04-06 20:09:30 +0700

    add path handling

--- src/main/feedback/message.php
+++ src/main/feedback/message.php
@@ -100,10 +100,10 @@
             // Display the formatted difference
             return $formattedDifference;
         }
-        public function toModal() {
+        public function toModal($currentPath = "./") {
             return '
-            <button data-modal-target="message'.$this->messageId.'-modal" data-modal-toggle="message'.$this->messageId.'-modal" class="w-full mb-1 text-gray-900 bg-blue-300 hover:bg-blue-400 px-5 py-2.5 text-start" type="button">
-              '.$this->title.'<span class="text-start"> '.$this->timeDifference().'</span>
+            <button data-modal-target="message'.$this->messageId.'-modal" data-modal-toggle="message'.$this->messageId.'-modal" class="w-full mb-1 font-medium text-gray-900 bg-blue-300 hover:bg-blue-400 px-5 py-2.5 text-start" type="button">
+              '.$this->title.'<span class="text-end font-normal"> '.$this->timeDifference().' ago</span>
             </button>
             
             <div id="message'.$this->messageId.'-modal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
@@ -129,8 +129,10 @@
                             </p>
                         </div>
                         <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b">
-                            <button data-modal-hide="message'.$this->messageId.'-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Update</button>
-                            <button data-modal-hide="message'.$this->messageId.'-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Delete</button>
+                            <form action="'.$currentPath.'delete-message.php" method="post">
+                                <input class="hidden" name="message_id" value="'.$this->messageId.'">
+                                <button data-modal-hide="message'.$this->messageId.'-modal" type="submit" class="py-2.5 px-5 ms-3 text-sm font-medium text-red-600 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-red-600 hover:text-white">Delete</button>
+                            </form>
                         </div>
                     </div>
                 </div>
